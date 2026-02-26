SaschaSchwarze0/knative-net-istio

Special handling for functions

pkg/reconciler/ingress/resources/virtual_service.go
@@ -170,16 +170,25 @@ func makeVirtualServiceRoute(hosts sets.String, http *v1alpha1.HTTPIngressPath, 
 			}
 		}
 
-		weights = append(weights, &istiov1alpha3.HTTPRouteDestination{
-			Destination: &istiov1alpha3.Destination{
-				Host: network.GetServiceHostname(
-					split.ServiceName, split.ServiceNamespace),
-				Port: &istiov1alpha3.PortSelector{
-					Number: uint32(split.ServicePort.IntValue()),
-				},
+		destination := &istiov1alpha3.Destination{
+			Host: network.GetServiceHostname(
+				split.ServiceName, split.ServiceNamespace),
+			Port: &istiov1alpha3.PortSelector{
+				Number: uint32(split.ServicePort.IntValue()),
 			},
-			Weight:  int32(split.Percent),
-			Headers: h,
+		}
+
+		if strings.HasSuffix(http.RewriteHost, ".function.cluster.local") {
+			destination.Host = "controller.codeengine-faas.svc.cluster.local"
+			destination.Port = &istiov1alpha3.PortSelector{
+				Number: 8000,
+			}
+		}
+
+		weights = append(weights, &istiov1alpha3.HTTPRouteDestination{
+			Destination: destination,
+			Weight:      int32(split.Percent),
+			Headers:     h,
 		})
 	}
 

src2img/knative-extensions-net-istio

pkg/reconciler/ingress/resources/virtual_service.go
@@ -169,16 +169,24 @@ func makeVirtualServiceRoute(hosts sets.Set[string], http *v1alpha1.HTTPIngressP
 			}
 		}
 
-		weights = append(weights, &istiov1beta1.HTTPRouteDestination{
-			Destination: &istiov1beta1.Destination{
-				Host: network.GetServiceHostname(
-					split.ServiceName, split.ServiceNamespace),
-				Port: &istiov1beta1.PortSelector{
-					//nolint:gosec // ignore integer overflow
-					Number: uint32(split.ServicePort.IntValue()),
-				},
+		destination := &istiov1beta1.Destination{
+			Host: network.GetServiceHostname(
+				split.ServiceName, split.ServiceNamespace),
+			Port: &istiov1beta1.PortSelector{
+				//nolint:gosec // ignore integer overflow
+				Number: uint32(split.ServicePort.IntValue()),
 			},
+		}
 
+		if strings.HasSuffix(http.RewriteHost, ".function.cluster.local") {
+			destination.Host = "controller.codeengine-faas.svc.cluster.local"
+			destination.Port = &istiov1beta1.PortSelector{
+				Number: 8000,
+			}
+		}
+
+		weights = append(weights, &istiov1beta1.HTTPRouteDestination{
+			Destination: destination,
 			//nolint:gosec // ignore integer overflow - percent is always >0 & <100
 			Weight:  int32(split.Percent),
 			Headers: h,